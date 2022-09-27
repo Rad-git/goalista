4da346534dde6999c849c3dd5581f4b7e6bbb16e
Radhouene ROUACHED <rrouached@altersis.local>
2022-09-27 10:39:46 +0100

    G-1: init

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config({ path: '.dev-env' });
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+
+// Serve frontend
+if (process.env.NODE_ENV === 'dev') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to dev'));
+}
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Goalista app
+
+## Usage
+
+add your MONGO_URI under dev-env
+
+### Install dependencies
+
+```
+# Backend deps
+npm install
+
+# Frontend deps
+cd frontend
+npm install
+```
+
+# Frontend deps
+cd frontend
+npm start
+```
+
+### Run Server
+
+```
+npm run server
+```
